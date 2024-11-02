a49a1aa6e3ef20042cee13f6e2844d764d6dd946
MrMrMops <ShestNik08@rambler.ru>
2024-11-02 03:37:31 +0300

    start menu

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
+import Header from "./components/Header";
+
 export default function App() {
   return (
     <>
-    <div className="bg-emerald-600 w-32 h-32">
-     <h1 className="text-3xl font-bold underline">
-       Hello world!
-     </h1>
-    </div>
+      <Header></Header>
     </>
   )
 }
