901011a3599becd2c604cf8f763285398d8d0097
MrMrMops <ShestNik08@rambler.ru>
2024-11-03 15:26:36 +0300

    banner and who us

---
+++ src/components/WhoUsOnly.jsx
@@ -0,0 +1,24 @@
+import apple from './images/apple.png'
+
+function Imageclick (event) {
+    event.target.
+}
+
+function whowindow () {
+    return (
+        <>
+            <div className='bg-gray-300 border-solid border-2'>
+                <p>я хусос занимаюсь хуйней я долбаеб</p>
+            </div>
+        </>
+    )
+}
+
+export default function WhoUs() {
+    return(
+        <>
+        <img className='hover:-translate-y-5 duration-75' src={apple} alt='apple'></img>
+        
+        
+        </>
+    )}
